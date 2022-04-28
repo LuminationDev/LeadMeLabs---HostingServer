4d86d6f113f2f4d3350871c015b6ee74359e087e
Edward Michael Coad <ecoad@lumination.com.au>
2022-04-28 14:56:05 +0930

    Initial upload

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+const compression = require('compression');
+const express = require('express');
+
+// Set up the Express App
+// =============================================================
+const app = express();
+const port = process.env.PORT || 8082;
+
+// Using compression npm to improve performance
+app.use(compression());
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes - may not be needed in the long run.
+// =============================================================
+require('./routes')(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
